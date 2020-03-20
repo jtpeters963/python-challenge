31f8b6314bda7fd939957dcd3ee4cdf75729f343
jtpeters963 <61399650+jtpeters963@users.noreply.github.com>
2020-03-20 18:24:59 -0400

    update using dictionaries

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import csv
 with open('budget_data.csv') as csv_file:
     budget = csv.reader(csv_file, delimiter=',')   #import the csv data
@@ -8,7 +7,7 @@ with open('budget_data.csv') as csv_file:
     prevyr = 0      #initialze the profit of a previous year holder
     biginc=0        #initial value for determining the month with the greatest increase
     bigdec=0        #initial value for determining the month with greatest decrease
-    headder = next(budget)    #strip the header
+    head = next(budget)    #strip the header
     for row in budget:                    #loop start
         income = income + float(row[1])    #calculate net income
         curyr = float(row[1])              # initialize current year's  profit/loss
@@ -22,10 +21,10 @@ with open('budget_data.csv') as csv_file:
             datedec = row[0]
         prevyr = curyr                      #stores this months profit for next step in loop
         x = x+1                             #count the months
-    avgchg = avgsum / (x-1)
+    avgchg = round(avgsum / (x-1),2)
     print(f"Total Months: {x}")             #print results to terminal
     print(f"Total: ${income}")
-    print(f"Average Change: ${round(avgchg,2)}")
+    print(f"Average Change: ${avgchg}")
     print(f"Createst Increase in Profits: {dateinc}(${biginc})")
     print(f"Greatest Decrease in Profits: {datedec}(${bigdec})")
     with open('budget.txt', 'w') as text:       #print results to text file budget.txt
@@ -34,41 +33,4 @@ with open('budget_data.csv') as csv_file:
         text.write(f"Average Change: ${round(avgchg,2)} \n")
         text.write(f"Createst Increase in Profits: {dateinc}(${biginc}) \n")
         text.write(f"Greatest Decrease in Profits: {datedec}(${bigdec}) \n")
-=======
-import csv
-with open('budget_data.csv') as csv_file:
-    budget = csv.reader(csv_file, delimiter=',')   #import the csv data
-    income = 0              #initialize accounting variables
-    x=0             #month counter
-    avgsum = 0      # sum for average change
-    prevyr = 0      #initialze the profit of a previous year holder
-    biginc=0        #initial value for determining the month with the greatest increase
-    bigdec=0        #initial value for determining the month with greatest decrease
-    headder = next(budget)    #strip the header
-    for row in budget:                    #loop start
-        income = income + float(row[1])    #calculate net income
-        curyr = float(row[1])              # initialize current year's  profit/loss
-        if x >0:                            # starts the calculation for average change on second month
-            avgsum = avgsum + curyr - prevyr
-        if curyr-prevyr > biginc:           #conditional for determining if this month had the greatest increase
-            biginc= curyr-prevyr
-            dateinc = row[0]
-        elif curyr-prevyr < bigdec:         #conditional for determining if this month had the greatest decrease
-            bigdec= curyr-prevyr
-            datedec = row[0]
-        prevyr = curyr                      #stores this months profit for next step in loop
-        x = x+1                             #count the months
-    avgchg = avgsum / (x-1)
-    print(f"Total Months: {x}")             #print results to terminal
-    print(f"Total: ${income}")
-    print(f"Average Change: ${round(avgchg,2)}")
-    print(f"Createst Increase in Profits: {dateinc}(${biginc})")
-    print(f"Greatest Decrease in Profits: {datedec}(${bigdec})")
-    with open('budget.txt', 'w') as text:       #print results to text file budget.txt
-        text.write(f"Total Months: {x} \n")
-        text.write(f"Total: ${income} \n")
-        text.write(f"Average Change: ${round(avgchg,2)} \n")
-        text.write(f"Createst Increase in Profits: {dateinc}(${biginc}) \n")
-        text.write(f"Greatest Decrease in Profits: {datedec}(${bigdec}) \n")
->>>>>>> fe3fce55be5f04456b922da48a6b0119f1540b52
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,79 +1,30 @@
-<<<<<<< HEAD
 import csv
-with open('election_data.csv') as csv_file:
+with open('../election_data.csv') as csv_file:
     polls = csv.reader(csv_file, delimiter=',') # import csv data
-    candidate=[]        #candidate list
-    votecnt = []        #votecount list for individual candidates
-    pctvt = []          #pctvt list for individual candidates
-    headder = next(polls)   #strip header
-    totvote=0    #initialzie counter for the total number of votes
+    candidate={}        #candidate dictionary initialization
+    totvote=0 
+    head = next(polls)   #strip header
     for row in polls:
         totvote = totvote+1  #count vote
-        isnew = True        # boolean for determining if this vote is for a new candidate
-        for x in range(len(candidate)):  #loop to check for candidates already in candidate list
-            if candidate[x] == row[2]:      
-                votecnt[x] = votecnt[x] + 1  #count vote for candidate in list
-                isnew = False               #sets boolean for new candidate to false
-        if isnew == True:                   #conditional to determine if this vote is for a new candidate
-           candidate.append(row[2])
-           votecnt.append(1)
-    for x in votecnt:                   #calculates percent of the vote for each candidate
-        pctvt.append(100*x/totvote)
+        if row[2] not in candidate:
+            candidate.update({row[2]:1})   #append dictionary with new candidates
+        else:
+             candidate[row[2]] = candidate[row[2]]+1    #counting votes for the candidate
     print("Election Results")               #printing results to terminal
     print("-----------------------")
     print(f"Total Votes: {totvote}")    
     print("------------------------")
-    wnnrpct = 0
-    for x in range(len(candidate)):
-        print(f"{candidate[x]}: {round(pctvt[x],2)}% ({votecnt[x]})")
-        if pctvt[x] > wnnrpct:
-            winner = candidate[x]
-            wnnrpct = pctvt[x]
+    winct = 0
+    for x in candidate:
+        print(f"{x}: {round(100*candidate[x]/totvote,2)}% ({candidate[x]})")
+        if candidate[x] > winct:
+            winner = x
+            winct = candidate[x]
     print("------------------------")
     print(f"The winner is {winner}")
 with open('election_results.txt', 'w') as text:   #printing results to text file election_results.txt
     text.write("Election Results \n")
     text.write(f"Total Votes: {totvote} \n")
-    for x in range(len(candidate)):
-        text.write(f"{candidate[x]}: {round(pctvt[x],3)}% ({votecnt[x]}) \n")
+    for x in candidate:
+        text.write(f"{x}: {round(100*candidate[x]/totvote,2)}% ({candidate[x]}) \n")
     text.write(f"The winner is {winner}!")
-=======
-import csv
-with open('election_data.csv') as csv_file:
-    polls = csv.reader(csv_file, delimiter=',') # import csv data
-    candidate=[]        #candidate list
-    votecnt = []        #votecount list for individual candidates
-    pctvt = []          #pctvt list for individual candidates
-    headder = next(polls)   #strip header
-    totvote=0    #initialzie counter for the total number of votes
-    for row in polls:
-        totvote = totvote+1  #count vote
-        isnew = True        # boolean for determining if this vote is for a new candidate
-        for x in range(len(candidate)):  #loop to check for candidates already in candidate list
-            if candidate[x] == row[2]:      
-                votecnt[x] = votecnt[x] + 1  #count vote for candidate in list
-                isnew = False               #sets boolean for new candidate to false
-        if isnew == True:                   #conditional to determine if this vote is for a new candidate
-           candidate.append(row[2])
-           votecnt.append(1)
-    for x in votecnt:                   #calculates percent of the vote for each candidate
-        pctvt.append(100*x/totvote)
-    print("Election Results")               #printing results to terminal
-    print("-----------------------")
-    print(f"Total Votes: {totvote}")    
-    print("------------------------")
-    wnnrpct = 0
-    for x in range(len(candidate)):
-        print(f"{candidate[x]}: {round(pctvt[x],2)}% ({votecnt[x]})")
-        if pctvt[x] > wnnrpct:
-            winner = candidate[x]
-            wnnrpct = pctvt[x]
-    print("------------------------")
-    print(f"The winner is {winner}")
-with open('election_results.txt', 'w') as text:   #printing results to text file election_results.txt
-    text.write("Election Results \n")
-    text.write(f"Total Votes: {totvote} \n")
-    for x in range(len(candidate)):
-        text.write(f"{candidate[x]}: {round(pctvt[x],3)}% ({votecnt[x]}) \n")
-    text.write(f"The winner is {winner}!")
->>>>>>> fe3fce55be5f04456b922da48a6b0119f1540b52
